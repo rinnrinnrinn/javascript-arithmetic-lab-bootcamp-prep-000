7b2db13aac863d97a4f86ca5a6d626ec47287bfe
Caitlin Lauer <caitlinrinn@gmail.com>
2018-04-24 01:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function divide(a, b){
   number = a / b;
   return number;
 }
-function inc(a) {
+function inc(n) {
   number++;
   return number;
 }
-function dec(a) {
+function dec(n) {
   number--;
   return number;
 }
